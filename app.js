b0b093660e2b85640684ded3bd2e5c2710578625
bedirgcmz <bdrgcmz@gmail.com>
2023-10-19 12:44:24 +0200

    Study English version 1 created

---
+++ app.js
@@ -0,0 +1,69 @@
+/* DOM elements */
+const renderContainer = document.getElementById("render-container");
+const getSentenceButton = document.getElementById("get-sentence");
+/* degiskenler */
+let data;
+let number = 0;
+
+async function fetchData() {
+  try {
+    const response = await fetch("data.json");
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    throw new Error("Veri alınamadı: " + error);
+  }
+}
+
+fetchData()
+  .then((jsonData) => {
+    data = jsonData;
+  })
+  .catch((error) => console.error("Hata:", error));
+
+const renderNewSentence = () => {
+  data &&
+    (renderContainer.innerHTML = `
+        <div class="sentence-container">
+            <div class="sentences-and-icon px-2 my-3">
+              <p>
+                ${data[number].sentence}
+              </p>
+              <i id=${data[number].id} onclick="openTranslate(this.id)" class="fa-solid fa-chevron-down open-icon px-2 mx-3"></i>
+              <i id=${data[number].id} onclick="openTranslate(this.id)" class="fa-solid fa-chevron-up close-icon px-2 mx-3 d-none"></i>
+            </div>
+            <p id="trl${data[number].id}" class="translate px-2 py-3">
+              ${data[number].translate}
+            </p>
+          </div>
+      `);
+  if (data) {
+    if (number < data.length - 1) {
+      number++;
+    } else {
+      number = 0;
+    }
+  }
+};
+const openTranslate = (id) => {
+  const targetElement = document.getElementById(`trl${id}`);
+  const openIcon = document.getElementById(id);
+  const closeIcon = document.querySelector(".close-icon");
+  if (
+    targetElement.style.display === "none" ||
+    targetElement.style.display === ""
+  ) {
+    targetElement.style.display = "block";
+    openIcon.classList.add("d-none");
+    closeIcon.classList.remove("d-none");
+  } else {
+    targetElement.style.display = "none";
+    openIcon.classList.remove("d-none");
+    closeIcon.classList.add("d-none");
+  }
+};
+
+/* Page loadind firstly add a sentence */
+renderNewSentence();
+/* This event get a new sentence */
+getSentenceButton.addEventListener("click", renderNewSentence);
