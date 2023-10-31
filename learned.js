0e9454f4a9c8b0742e60f5c2bde1c7dda1ca8e9c
bedirgcmz <bdrgcmz@gmail.com>
2023-10-31 09:47:40 +0100

    deleyted comments

--- learned.js
+++ learned.js
@@ -10,41 +10,6 @@ learnedData = getLocalStorage("allSentencesData").filter(
 );
 learnedWordsNumber.innerText = learnedData ? learnedData.length : 0;
 
-// const renderNewSentenceLearned = (number) => {
-//   totalSentencesLearned.innerText = `${number + 1}/${learnedData.length}`;
-
-//   if (learnedData.length > 0) {
-//     learnedData &&
-//       (renderContainerLearned.innerHTML = `
-//           <div class="sentence-container">
-//               <div class="sentences-and-icon px-2 my-3">
-//                 <p>
-//                   ${learnedData[number].sentence}
-//                 </p>
-//                 <div class="d-flex flex-column align-items-center justfy-content-end">
-//                   <i id="${learnedData[number].id}-learned" onclick="openTranslateLearned(this.id)" class="fa-solid fa-chevron-down open-icon-learned px-2 mx-1 mb-2"></i>
-//                   <i id="${learnedData[number].id}-learned" onclick="openTranslateLearned(this.id)" class="fa-solid fa-chevron-up close-icon-learned px-2 mx-1 mb-2 d-none"></i>
-//                   <i id="${learnedData[number].id}-learned" onclick="readText(this.id)" class="fa-solid fa-volume-high read-icon-learned p-2 mx-1 mb-2"></i>
-//                   <i id=${learnedData[number].id} onclick="forgotSentence(${learnedData[number].id})" class="fa-solid fa-xmark unlearned-icon"></i>
-//                 </div>
-//               </div>
-//               <p id="trl${learnedData[number].id}-learned" class="translate px-2 py-3">
-//                 ${learnedData[number].translate}
-//               </p>
-//             </div>
-//         `);
-//   } else {
-//     learnedData &&
-//       (renderContainerLearned.innerHTML = `
-//           <div class="sentence-container p-2">
-//                 <p>
-//                   You haven't learned the sentence yet!
-//                 </p>
-//             </div>
-//         `);
-//   }
-// };
-
 /**when loading page */
 renderNewSentenceLearned(0);
 
@@ -78,8 +43,6 @@ const getPrevSentenceLearned = () => {
     } else {
       number--;
       renderNewSentenceLearned(number);
-      console.log(learnedData.length);
-      console.log(number);
     }
   }
 };
